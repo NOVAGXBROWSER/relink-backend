d5621d13c2735017f5fbac3389975e5a2e155515
NOVAGXBROWSER <shockinggamesbusiness@gmail.com>
2025-09-27 18:50:50 -0400

    Add files via upload

---
+++ RELINKBACKEND.js
@@ -0,0 +1,65 @@
+// Minimal RELINK chat prototype
+// Backend: Node.js + Express + Socket.IO
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // Serve frontend files
+
+// Store rooms and users in memory
+const rooms = {};
+
+io.on('connection', (socket) => {
+  let currentRoom = null;
+  let username = null;
+
+  socket.on('setUsername', (name) => {
+    username = name;
+    socket.emit('usernameSet', name);
+  });
+
+  socket.on('createRoom', (roomName) => {
+    if (!rooms[roomName]) {
+      rooms[roomName] = [];
+    }
+    socket.join(roomName);
+    currentRoom = roomName;
+    rooms[roomName].push(username);
+    io.to(currentRoom).emit('userList', rooms[currentRoom]);
+    socket.emit('roomJoined', roomName);
+  });
+
+  socket.on('joinRoom', (roomName) => {
+    if (rooms[roomName]) {
+      socket.join(roomName);
+      currentRoom = roomName;
+      rooms[roomName].push(username);
+      io.to(currentRoom).emit('userList', rooms[currentRoom]);
+      socket.emit('roomJoined', roomName);
+    } else {
+      socket.emit('error', 'Room does not exist');
+    }
+  });
+
+  socket.on('sendMessage', (msg) => {
+    if (currentRoom) {
+      io.to(currentRoom).emit('newMessage', { username, msg });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    if (currentRoom && rooms[currentRoom]) {
+      rooms[currentRoom] = rooms[currentRoom].filter(user => user !== username);
+      io.to(currentRoom).emit('userList', rooms[currentRoom]);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('RELINK backend running on http://localhost:3000');
+});
